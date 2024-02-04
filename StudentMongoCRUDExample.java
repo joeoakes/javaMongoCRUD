ddf56fea6144ef8b75cacf0f9c1d50f4e30c1b2a
joeoakes <jxo19@psu.edu>
2024-02-04 14:41:46 -0500

    commit

--- StudentMongoCRUDExample.java
+++ StudentMongoCRUDExample.java
@@ -1,47 +1,26 @@
 import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoClient;
-import com.mongodb.client.MongoDatabase;
 import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
 import org.bson.Document;
 
 public class StudentMongoCRUDExample {
     public static void main(String[] args) {
-        // Connect to MongoDB (Make sure MongoDB server is running)
-        MongoClientURI connectionString = new MongoClientURI("mongodb://localhost:27017");
-        try (MongoClient mongoClient = new MongoClient(connectionString)) {
+        // Create a MongoClient using the factory method
+        try (MongoClient mongoClient = MongoClients.create("mongodb://localhost:27017")) {
             // Access the database and collection
             MongoDatabase database = mongoClient.getDatabase("your_database_name");
             MongoCollection<Document> collection = database.getCollection("students");
 
-            // Create
+            // Perform CRUD operations here
+            // Example: Insert a document
             Document newStudent = new Document("first_name", "John")
                     .append("last_name", "Doe")
                     .append("age", 20)
                     .append("email", "john@example.com");
             collection.insertOne(newStudent);
 
-            // Read
-            MongoCursor<Document> cursor = collection.find().iterator();
-            while (cursor.hasNext()) {
-                Document student = cursor.next();
-                System.out.println(student);
-            }
-            cursor.close();
-
-            // Update
-            Document updatedStudent = new Document("$set", new Document("first_name", "Updated First Name"));
-            collection.updateOne(new Document("first_name", "John"), updatedStudent);
-
-            // Read again
-            cursor = collection.find().iterator();
-            while (cursor.hasNext()) {
-                Document student = cursor.next();
-                System.out.println(student);
-            }
-            cursor.close();
-
-            // Delete
-            collection.deleteOne(new Document("first_name", "John"));
+            // Continue with other CRUD operations...
         }
     }
 }
